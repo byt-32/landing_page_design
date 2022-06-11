d072f60498e0d525bed52230aa2dc80f4809b46f
byt-32 <null@gmail.com>
2022-06-11 13:19:03 -0800

    add new files

---
+++ src/assets/script/script.js
@@ -0,0 +1,34 @@
+(function() {
+	const parent = document.querySelector('#showOnScroll')
+	const observed = document.querySelectorAll('#showOnScroll > div')
+
+	window.addEventListener('DOMContentLoaded', (e) => {
+		window.IntersectionObserver !== undefined && assignIntialProps()
+	})
+	// window.addEventListener('scroll', showElements)
+
+	function assignIntialProps() {
+		const observer = new IntersectionObserver(showElements,
+			{root: null, rootMargin: '0px', threshold: .4}
+		)
+		Array.from(observed).forEach(e => {
+			e.style.visibility = 'hidden'
+			e.style.opacity = 0
+			observer.observe(e)
+		})
+		
+	};
+	function showElements(arr) {
+		arr.forEach((ele) => {
+			const observed = ele
+
+			if (observed.isIntersecting) {
+				observed.target.style.visibility = 'visible'
+				observed.target.style.opacity = 1
+			}
+		})
+	}
+
+	
+
+})()
